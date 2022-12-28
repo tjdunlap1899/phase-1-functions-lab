ed76623c7d30c169df97c81190a70bfe789a8f56
Trevor Dunlap <tjdunlap1899@gmail.com>
2022-12-28 16:04:02 -0500

    commit tree b93aee0e580350e6ea2fb30b1ffe76fead1b6526

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+     if (distance > 42){
+        return distance - 42;
+     } else {
+        return 42 - distance;
+     }
+}
+
+function distanceFromHqInFeet(distance){
+    return distanceFromHqInBlocks(distance) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    if (start < destination) {
+        return (destination - start) * 264;
+    } else {
+        return (start - destination) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400){
+        return 0;
+    }else if (distance > 400 && distance <= 2000){
+        return .02 * (distance - 400);
+    }else if (distance > 2000 && distance < 2500){
+        return 25;
+    }else {
+        return 'cannot travel that far';
+    }
+}
